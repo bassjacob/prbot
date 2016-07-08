6eedf9696fa05bb5b9dad49cf8768e31a0c1d062
Jacob Bass <jacob@jacobbass.net>
2016-07-09 00:26:15 +1000

    get first page

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,36 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib ( someFunc
+           ) where
+
+import Network.Wreq
+import Control.Lens
+import Control.Monad
+import Data.Aeson
+import Data.Aeson.Lens
+import qualified System.Environment as E (lookupEnv)
+import qualified Data.ByteString.Char8 as B (ByteString, pack)
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Internal as BLI
+
+apiURL = "https://api.github.com/search/repositories?q=.dotfiles+user:subshad"
+
+getRepos :: Maybe String -> IO (Maybe [Value])
+getRepos Nothing = return Nothing
+getRepos (Just token) = do
+  response <- getWith opts apiURL
+  let repos = response
+              ^.. responseBody
+              . key "items"
+              . _Array
+              . traverse
+              . key "name"
+  return $ Just repos
+    where
+      opts = defaults & auth ?~ oauth2Token (B.pack token)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  token <- E.lookupEnv "GITHUB_TOKEN"
+  status <- getRepos token
+  print status
