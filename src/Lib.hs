6ac592cb3d4b7f3b1379acf1a90f54309f7b7f08
Jacob Bass <jacob@jacobbass.net>
2016-07-09 01:48:58 +1000

    cli args and get all repos

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,29 +8,39 @@ import Control.Lens
 import Control.Monad
 import Data.Aeson
 import Data.Aeson.Lens
-import qualified System.Environment as E (lookupEnv)
-import qualified Data.ByteString.Char8 as B (ByteString, pack)
+import qualified System.Environment as E (lookupEnv, getArgs)
+import qualified Data.ByteString.Char8 as B (ByteString, pack, unpack)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Internal as BLI
 
-apiURL = "https://api.github.com/search/repositories?q=.dotfiles+user:subshad"
+apiUrl :: [String] -> Maybe String
+apiUrl [] = Nothing
+apiUrl [_] = Nothing
+apiUrl (name:user:_) = Just $ "https://api.github.com/search/repositories?q="
+                            ++ name
+                            ++ "+user:"
+                            ++ user
 
-getRepos :: Maybe String -> IO (Maybe [Value])
-getRepos Nothing = return Nothing
-getRepos (Just token) = do
-  response <- getWith opts apiURL
+getRepos :: Maybe String -> Maybe String -> IO (Maybe [Value])
+getRepos _ Nothing = return Nothing
+getRepos Nothing _ = return $ Just []
+getRepos (Just url) (Just token) = do
+  response <- getWith opts url
+  let next = response ^? responseLink "rel" "next" . linkURL
+  nextRepos <- getRepos (B.unpack <$> next) (Just token)
   let repos = response
               ^.. responseBody
               . key "items"
               . _Array
               . traverse
               . key "name"
-  return $ Just repos
+  return $ liftM2 (++) (Just repos) nextRepos
     where
       opts = defaults & auth ?~ oauth2Token (B.pack token)
 
 someFunc :: IO ()
 someFunc = do
   token <- E.lookupEnv "GITHUB_TOKEN"
-  status <- getRepos token
-  print status
+  args <- E.getArgs
+  repos <- getRepos (apiUrl args) token
+  print repos
