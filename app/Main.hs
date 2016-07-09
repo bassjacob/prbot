84ebbd319ad3cec16b451ad92474ddb029a45ab4
Jacob Bass <jacob@jacobbass.net>
2016-07-09 13:51:21 +1000

    refactor into smaller functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
+import System.Environment (lookupEnv, getArgs)
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  token <- lookupEnv "GITHUB_TOKEN"
+  args <- getArgs
+  repos <- someFunc token args
+  print repos
--- src/Lib.hs
+++ src/Lib.hs
@@ -8,39 +8,38 @@ import Control.Lens
 import Control.Monad
 import Data.Aeson
 import Data.Aeson.Lens
-import qualified System.Environment as E (lookupEnv, getArgs)
 import qualified Data.ByteString.Char8 as B (ByteString, pack, unpack)
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Internal as BLI
 
-apiUrl :: [String] -> Maybe String
-apiUrl [] = Nothing
-apiUrl [_] = Nothing
-apiUrl (name:user:_) = Just $ "https://api.github.com/search/repositories?q="
-                            ++ name
-                            ++ "+user:"
-                            ++ user
+apiUrl :: String -> String -> String
+apiUrl search user =
+  "https://api.github.com/search/repositories?q="
+    ++ search
+    ++ "+user:"
+    ++ user
 
-getRepos :: Maybe String -> Maybe String -> IO (Maybe [Value])
-getRepos _ Nothing = return Nothing
+getPage url token =
+  let opts = defaults & auth ?~ oauth2Token (B.pack token) in
+    getWith opts url
+
+extractRepos = flip (^..) $ responseBody
+                              . key "items"
+                              . _Array
+                              . traverse
+                              . key "name"
+
+getRepos :: Maybe String -> String -> IO (Maybe [Value])
 getRepos Nothing _ = return $ Just []
-getRepos (Just url) (Just token) = do
-  response <- getWith opts url
-  let next = response ^? responseLink "rel" "next" . linkURL
-  nextRepos <- getRepos (B.unpack <$> next) (Just token)
-  let repos = response
-              ^.. responseBody
-              . key "items"
-              . _Array
-              . traverse
-              . key "name"
+getRepos (Just url) token = do
+  response <- getPage url token
+  let repos = extractRepos response
+  let next = B.unpack <$> response ^? responseLink "rel" "next" . linkURL
+  nextRepos <- getRepos next token
   return $ liftM2 (++) (Just repos) nextRepos
-    where
-      opts = defaults & auth ?~ oauth2Token (B.pack token)
 
-someFunc :: IO ()
-someFunc = do
-  token <- E.lookupEnv "GITHUB_TOKEN"
-  args <- E.getArgs
-  repos <- getRepos (apiUrl args) token
-  print repos
+someFunc :: Maybe String -> [String] -> IO (Maybe [Value])
+someFunc Nothing _ = return Nothing
+someFunc _ [] = return Nothing
+someFunc _ [_] = return Nothing
+someFunc (Just token) (search:user:_) = getRepos (Just (apiUrl search user)) token
