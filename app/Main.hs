36a311673ab62b2e9a5f2ff2541622d8b957dc90
Jacob Bass <jacob@jacobbass.net>
2016-07-11 11:55:25 +1000

    use sequence to return an IO wrapped list of maybe prs

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
 module Main where
 
 import Env
 import Lib
+import Data.Aeson
+import qualified Data.Text as T
 
 data Opts = Opts { token :: String
                  , user :: String
@@ -15,6 +18,10 @@ optsParse = Env.parse (header "prbot") $ Opts <$> var (str <=< nonempty) "GITHUB
                                               <*> var (str <=< nonempty) "GITHUB_USER"   (help "github user or organisation to scope repos to" <> keep)
                                               <*> var (str <=< nonempty) "GITHUB_SEARCH" (help "search string to limit prs to resultset" <> keep)
 
+extractRepos Nothing = []
+extractRepos (Just []) = []
+extractRepos (Just ((String x):xs)) = (T.unpack x) : extractRepos (Just xs)
+
 main :: IO ()
 main = do
   Opts { token
@@ -24,6 +31,6 @@ main = do
 
   repos <- getUserRepos token user search
   print $ "Finding PRs for " ++ show (fmap length repos) ++ " Repositories"
-  prs <- getRepoPullRequests token user "fe-ops-agency-server"
+  prs <- sequence (fmap (getRepoPullRequests token user) (extractRepos (fmap (take 10) repos)))
   print prs
 
